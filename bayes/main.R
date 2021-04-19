f11a99e8c9a8d91303d4bb025b0db9277477b172
longyyu <longyyu@umich.edu>
2021-04-20 01:38:47 +0800

    Add hierarchical model (v1)

--- bayes/main.R
+++ bayes/main.R
@@ -3,11 +3,12 @@
 # Updated: Apr 18, 2021
 
 # Set up ----------------------------------------------------------------------
-run_level = 1 # 1 for trial run, 2 for official run
+run_level = 2 # 1 for trial run, 2 for official run
 N_WARMUP = switch(run_level, 300, 1000)
 N_ITER = switch(run_level, 1000, 3000)
-N_CORE = switch(run_level, 1L, 4L)
-EXPORT_FILE = switch(run_level, "fit_logit_trial", "fit_logit_official")
+N_CORE = switch(run_level, 1L, 12L)
+EXPORT_FILE = switch(run_level, "fit_logit_rl1", "fit_logit_rl2")
+EXPORT_FILE_HIER = switch(run_level, "fit_logit_hier_rl1", "fit_logit_hier_rl2")
 
 suppressPackageStartupMessages({
   library(tidyverse)
@@ -29,7 +30,7 @@ compute_ber = function(yhat, ytrue) {
 load("data/windstorm_weather_prev_month.RData")
 data_merged = data_merged %>%
   mutate(outcome = ifelse(num_episodes > 0, 1, 0)) %>% 
-  select(-ym, -city, -county, -state, -num_episodes) %>%
+  select(-ym, -city, -county, -num_episodes) %>%
   drop_na()
 
 data_x = data_merged %>% 
@@ -37,6 +38,60 @@ data_x = data_merged %>%
          wind_speed_avg, wind_speed_sd) %>% 
   as.matrix() %>% scale()
 K = ncol(data_x)
+states = as.factor(data_merged$state) # levels(states)
+
+# the hierarchical logistic model ---------------------------------------------
+
+stan_data_hier = list(
+  N = nrow(data_x),
+  K = K,
+  G = length(levels(states)),
+  y = data_merged$outcome,
+  X = data_x,
+  group = as.numeric(states),
+  alpha_mean = 0,
+  alpha_s2 = 10,
+  a_mean = 0,
+  a_s2 = 5,
+  beta_mean = rep(0, K),
+  beta_s2 = rep(2.5, K)
+)
+
+# MCMC simulation ------------
+if (dir("bayes/", pattern = EXPORT_FILE_HIER) %>% length() == 0) {
+  # only re-fit the model when EXPORT_FILEs are not found
+  fit_logit_hier = stan(
+    "bayes/logit_hierarchical.stan", data = stan_data_hier, 
+    pars = c("alpha", "a", "beta", "y_rep", "rho"), # not saving eta
+    warmup = N_WARMUP, iter = N_ITER, refresh = 1000, cores = N_CORE,
+    sample_file = sprintf("bayes/%s.csv", EXPORT_FILE_HIER)
+  )
+} # if
+
+system.time({ # 30sec
+  fit_logit_hier = read_stan_csv(
+    dir("bayes/", pattern = EXPORT_FILE_HIER, full.names = TRUE)
+  )
+})
+
+# Results --------------
+traceplot(fit_logit_hier, c("alpha", "beta"), inc_warmup = TRUE)
+traceplot(fit_logit_hier, c("a"), inc_warmup = TRUE)
+
+fit_summary = summary(fit_logit_hier, pars = c("alpha", "beta"), probs = c(0.025, 0.975))$summary
+fit_summary # %>% knitr::kable(digits = 3)
+
+fit_summary = summary(fit_logit_hier, pars = c("a"), probs = c(0.025, 0.975))$summary
+fit_summary # %>% knitr::kable(digits = 3)
+
+plot(fit_logit, pars = c("alpha", "beta"))
+library(bayesplot)
+color_scheme_set("blue")
+mcmc_intervals(as.matrix(fit_logit, pars = c("alpha", "beta")), prob = 0.95)
+ppc_dens_overlay(stan_data$y, as.matrix(fit_logit, pars = "y_rep")[1:150,])
+
+
+# the basic logistic model ----------------------------------------------------
 
 stan_data = list(
   N = nrow(data_x),
@@ -49,13 +104,13 @@ stan_data = list(
   beta_s2 = rep(2.5, K)
 )
 
-# MCMC simulation -------------------------------------------------------------
+# MCMC simulation ---------
 if (dir("bayes/", pattern = EXPORT_FILE) %>% length() == 0) {
   # only re-fit the model when EXPORT_FILEs are not found
   fit_logit = stan(
     "bayes/logit.stan", data = stan_data, 
     pars = c("alpha", "beta", "y_rep", "rho"), # not saving eta
-    warmup = 300, iter = 1000, refresh = 1000,
+    warmup = N_WARMUP, iter = N_ITER, refresh = 1000, cores = N_CORE,
     sample_file = sprintf("bayes/%s.csv", EXPORT_FILE)
   )
 } # if
@@ -66,11 +121,12 @@ system.time({ # 30sec
   )
 })
 
-# Results ---------------------------------------------------------------------
+# Results --------------
 traceplot(fit_logit, c("alpha", "beta"), inc_warmup = TRUE)
 
 get_posterior_mean(fit_logit, pars = c("alpha", "beta"))[,5]
-print(fit_logit, c("alpha", "beta"),  probs = c(0.025, 0.975))
+fit_summary = summary(fit_logit, pars = c("alpha", "beta"), probs = c(0.025, 0.975))$summary
+fit_summary # %>% knitr::kable(digits = 3)
 
 plot(fit_logit, pars = c("alpha", "beta"))
 library(bayesplot)
